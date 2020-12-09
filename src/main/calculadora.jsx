8bd06b2678829c97634759caeb801565572e6ffc
Pedro Benevides <pedrobenevides3243@gmail.com>
2020-12-09 20:32:25 -0300

    criado base da calculadora

---
+++ src/main/calculadora.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './calculadora.css';
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            <div className='calculator'>
+
+            </div>
+        )
+    }
+}
